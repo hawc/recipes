6215b17bc1b8eab16fbbb16e8b38f5da6d4d9a99
Hendrik Wichern <git@hendrikwichern.de>
2023-01-15 17:43:20 +0100

    share debugging

--- src/pages/receipts/[id].js
+++ src/pages/receipts/[id].js
@@ -1,7 +1,13 @@
 import { loadPost, loadPosts } from '@/lib/contentful';
-import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import styles from '@/styles/Detail.module.css';
 import { useState } from 'react';
+import { BLOCKS, INLINES } from "@contentful/rich-text-types";
+
+// Create a bespoke renderOptions object to target BLOCKS.EMBEDDED_ENTRY (linked block entries e.g. code blocks)
+// INLINES.EMBEDDED_ENTRY (linked inline entries e.g. a reference to another blog post)
+// and BLOCKS.EMBEDDED_ASSET (linked assets e.g. images)
+
 
 export async function getStaticPaths() {
   if (process.env.SKIP_BUILD_STATIC_GENERATION) {
@@ -31,9 +37,10 @@ export async function getStaticProps({ params }) {
 export default function Receipt({ post }) {
   const postdata = JSON.parse(post);
   const [servings, setServings] = useState(1);
+  console.log(postdata.fields)
   const ingredients = postdata.fields.ingredients?.map((ingredient) => (
     <li key={ingredient.fields.name}>
-      {ingredient.fields.amount * servings}
+      {ingredient.fields.amount * servings}&nbsp;
       {ingredient.fields.measurement} {ingredient.fields.name}
     </li>
   ));
@@ -44,6 +51,75 @@ export default function Receipt({ post }) {
   function handleServingsChange(e) {
     setServings(e.target.value);
   }
+
+  function share(event) {
+    console.log(navigator)
+    if (!navigator || typeof (navigator.canShare) !== 'function') {
+      return;
+    }
+    navigator.share({text: "event.currentTarget.innerHTML"})
+      .then(() => {
+        console.log('Successful share');
+      })
+      .catch(error => {
+        console.log(navigator)
+        if (error.name === 'AbortError') {
+          console.log('Share card was probably just dismissed');
+          return;
+        }
+
+        console.warn(error);
+      });
+  }
+
+  const renderOptions = {
+    renderNode: {
+      [INLINES.EMBEDDED_ENTRY]: (node, children) => {
+        // target the contentType of the EMBEDDED_ENTRY to display as you need
+        if (node.data.target.sys.contentType.sys.id === "ingredient") {
+          return (
+            <span>{node.data.target.fields.amount * servings} {node.data.target.fields.name}</span>
+          );
+        }
+      },
+      [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
+        // target the contentType of the EMBEDDED_ENTRY to display as you need
+        if (node.data.target.sys.contentType.sys.id === "codeBlock") {
+          return (
+            <pre>
+              <code>{node.data.target.fields.code}</code>
+            </pre>
+          );
+        }
+
+        if (node.data.target.sys.contentType.sys.id === "videoEmbed") {
+          return (
+            <iframe
+              src={node.data.target.fields.embedUrl}
+              height="100%"
+              width="100%"
+              frameBorder="0"
+              scrolling="no"
+              title={node.data.target.fields.title}
+              allowFullScreen={true}
+            />
+          );
+        }
+      },
+
+      [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
+        // render the EMBEDDED_ASSET as you need
+        return (
+          <img
+            src={`https://${node.data.target.fields.file.url}`}
+            height={node.data.target.fields.file.details.image.height}
+            width={node.data.target.fields.file.details.image.width}
+            alt={node.data.target.fields.description}
+          />
+        );
+      },
+    },
+  };
 
   return (
     <div>
@@ -59,13 +135,9 @@ export default function Receipt({ post }) {
         />
       </div>
       <h3>Zutaten</h3>
-      <ul className={styles.ingredients}>{ingredients}</ul>
+      <ul className={styles.ingredients} onClick={share}>{ingredients}</ul>
       <h3>Zubereitung</h3>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: documentToHtmlString(postdata.fields.description),
-        }}
-      ></div>
+      <div>{documentToReactComponents(postdata.fields.description, renderOptions)}</div>
     </div>
   );
 }
