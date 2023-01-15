2eafe91030013d775e11dcb9bef676849359c602
Hendrik Wichern <git@hendrikwichern.de>
2023-01-15 17:45:15 +0100

    code formatting

--- src/pages/receipts/[id].js
+++ src/pages/receipts/[id].js
@@ -2,12 +2,11 @@ import { loadPost, loadPosts } from '@/lib/contentful';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import styles from '@/styles/Detail.module.css';
 import { useState } from 'react';
-import { BLOCKS, INLINES } from "@contentful/rich-text-types";
+import { BLOCKS, INLINES } from '@contentful/rich-text-types';
 
 // Create a bespoke renderOptions object to target BLOCKS.EMBEDDED_ENTRY (linked block entries e.g. code blocks)
 // INLINES.EMBEDDED_ENTRY (linked inline entries e.g. a reference to another blog post)
 // and BLOCKS.EMBEDDED_ASSET (linked assets e.g. images)
-
 
 export async function getStaticPaths() {
   if (process.env.SKIP_BUILD_STATIC_GENERATION) {
@@ -37,7 +36,7 @@ export async function getStaticProps({ params }) {
 export default function Receipt({ post }) {
   const postdata = JSON.parse(post);
   const [servings, setServings] = useState(1);
-  console.log(postdata.fields)
+  console.log(postdata.fields);
   const ingredients = postdata.fields.ingredients?.map((ingredient) => (
     <li key={ingredient.fields.name}>
       {ingredient.fields.amount * servings}&nbsp;
@@ -53,16 +52,17 @@ export default function Receipt({ post }) {
   }
 
   function share(event) {
-    console.log(navigator)
-    if (!navigator || typeof (navigator.canShare) !== 'function') {
+    console.log(navigator);
+    if (!navigator || typeof navigator.canShare !== 'function') {
       return;
     }
-    navigator.share({text: "event.currentTarget.innerHTML"})
+    navigator
+      .share({ text: 'event.currentTarget.innerHTML' })
       .then(() => {
         console.log('Successful share');
       })
-      .catch(error => {
-        console.log(navigator)
+      .catch((error) => {
+        console.log(navigator);
         if (error.name === 'AbortError') {
           console.log('Share card was probably just dismissed');
           return;
@@ -76,15 +76,18 @@ export default function Receipt({ post }) {
     renderNode: {
       [INLINES.EMBEDDED_ENTRY]: (node, children) => {
         // target the contentType of the EMBEDDED_ENTRY to display as you need
-        if (node.data.target.sys.contentType.sys.id === "ingredient") {
+        if (node.data.target.sys.contentType.sys.id === 'ingredient') {
           return (
-            <span>{node.data.target.fields.amount * servings} {node.data.target.fields.name}</span>
+            <span>
+              {node.data.target.fields.amount * servings}{' '}
+              {node.data.target.fields.name}
+            </span>
           );
         }
       },
       [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
         // target the contentType of the EMBEDDED_ENTRY to display as you need
-        if (node.data.target.sys.contentType.sys.id === "codeBlock") {
+        if (node.data.target.sys.contentType.sys.id === 'codeBlock') {
           return (
             <pre>
               <code>{node.data.target.fields.code}</code>
@@ -92,7 +95,7 @@ export default function Receipt({ post }) {
           );
         }
 
-        if (node.data.target.sys.contentType.sys.id === "videoEmbed") {
+        if (node.data.target.sys.contentType.sys.id === 'videoEmbed') {
           return (
             <iframe
               src={node.data.target.fields.embedUrl}
@@ -135,9 +138,13 @@ export default function Receipt({ post }) {
         />
       </div>
       <h3>Zutaten</h3>
-      <ul className={styles.ingredients} onClick={share}>{ingredients}</ul>
+      <ul className={styles.ingredients} onClick={share}>
+        {ingredients}
+      </ul>
       <h3>Zubereitung</h3>
-      <div>{documentToReactComponents(postdata.fields.description, renderOptions)}</div>
+      <div>
+        {documentToReactComponents(postdata.fields.description, renderOptions)}
+      </div>
     </div>
   );
 }
