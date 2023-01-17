f9d6731f2b22eb7b89a39a1f48ce7f7489fe8e8f
Hendrik Wichern <git@hendrikwichern.de>
2023-01-17 10:58:48 +0100

    added link preview

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
 import Link from 'next/link';
 import { loadPosts } from '@/lib/contentful';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useState, createRef } from 'react';
+import Image from 'next/image';
+import { EyeIcon } from '@heroicons/react/24/solid';
+import { useMediaQuery } from 'react-responsive';
+
+function useDesktopMediaQuery() {
+  return useMediaQuery({
+    minWidth: 769,
+  });
+}
 
 export async function getStaticProps() {
   const posts = await loadPosts(`receipt`);
@@ -28,20 +37,68 @@ interface ReceiptFields {
 }
 
 export default function Home({ posts, categories }) {
+  const [previewImage, setPreviewImage] = useState(null);
+
   const postdata = JSON.parse(posts);
   const categorydata = JSON.parse(categories);
   const [filteredPosts, setFilteredPosts] = useState(postdata);
+
+  function Mobile({ children }) {
+    return <>{useDesktopMediaQuery() ? null : children}</>;
+  }
+
+  function handleReceiptHover(id) {
+    if (!id) {
+      setPreviewImage(null);
+      return;
+    }
+
+    const previewImageObject = postdata.find(
+      (receipt) => receipt.sys.id === id,
+    );
+    const previewImageSrc = previewImageObject?.fields.images[0]?.fields.file;
+    setPreviewImage(previewImageSrc);
+  }
+
+  const postRefs = {};
 
   const postListItems = postdata.map((post: Receipt) => {
     const isFiltered = filteredPosts
       .map((filteredPost) => filteredPost.sys.id)
       .includes(post.sys.id);
     const opacityClass = isFiltered ? `is-size-5` : `is-size-5 opacity-50`;
+    postRefs[post.sys.id] = createRef();
     return (
-      <li key={post.fields.name} className={opacityClass}>
-        <Link className="has-text-primary" href={`/rezept/${post.fields.slug}`}>
+      <li
+        onMouseEnter={() => handleReceiptHover(post.sys.id)}
+        onMouseLeave={() => handleReceiptHover(null)}
+        key={post.sys.id}
+        ref={postRefs[post.sys.id]}
+        className={opacityClass}
+      >
+        <Link
+          onFocus={() => handleReceiptHover(post.sys.id)}
+          onBlur={() => handleReceiptHover(null)}
+          className="has-text-primary"
+          href={`/rezept/${post.fields.slug}`}
+        >
           {post.fields.name}
         </Link>
+        <Mobile>
+          <button
+            type="button"
+            className="button is-white is-small ml-1"
+            onFocus={() => handleReceiptHover(post.sys.id)}
+            onBlur={() => handleReceiptHover(null)}
+            onClick={() => {
+              postRefs[post.sys.id].current.children[0]?.focus();
+            }}
+          >
+            <span className="icon is-medium">
+              <EyeIcon />
+            </span>
+          </button>
+        </Mobile>
       </li>
     );
   });
@@ -77,7 +134,24 @@ export default function Home({ posts, categories }) {
             </select>
           </div>
         </h2>
-        <ul>{postListItems}</ul>
+        <div className="columns">
+          <div className="column">
+            <ul>{postListItems}</ul>
+          </div>
+          <div className="column">
+            {previewImage && (
+              <div>
+                <Image
+                  src={`https://${previewImage.url}`}
+                  className="box p-0"
+                  alt="Rezeptvorschau"
+                  width={previewImage.details.image.width}
+                  height={previewImage.details.image.height}
+                ></Image>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </section>
   );
