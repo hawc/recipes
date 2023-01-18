b46c6f57b46e576a65d42d26dc49e7ebe4eb822a
Hendrik Wichern <git@hendrikwichern.de>
2023-01-18 19:49:09 +0100

    content interface test

--- src/lib/contentfulManagement.ts
+++ src/lib/contentfulManagement.ts
@@ -4,13 +4,20 @@ export function initManagement() {
   const client = contentful.createClient({
     accessToken: process.env.contentfulManagementAccessToken,
   });
-
-  const contents = client
+  client
     .getSpace(process.env.contentfulSpace)
     .then((space) => space.getEnvironment(`master`))
-    .then((environment) => environment.getContentTypes())
-    .then((response) => console.log(response.items))
+    .then((environment) =>
+      environment.getEditorInterfaceForContentType(`receipt`),
+    )
+    .then((editorInterface) => console.log(editorInterface))
     .catch(console.error);
+  // const contents = client
+  //   .getSpace(process.env.contentfulSpace)
+  //   .then((space) => space.getEnvironment(`master`))
+  //   .then((environment) => environment.getContentTypes())
+  //   .then((response) => console.log(response.items))
+  //   .catch(console.error);
 
-  return contents;
+  return true;
 }
